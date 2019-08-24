47f06c41323e72db4fbf27a4c3288080edd14d61
Treagan Birbal <treaganbirbal@pursuit.org>
2019-08-24 18:36:03 -0400

    created Planets/js

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ import Home from './components/Home.js'
 import Films from './components/Films.js'
 import People from './components/People.js'
 import SinglePerson from './components/singlePerson.js'
+import Planets from './components/Planets.js'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="">
       <Navbar />
       <Switch>
+        <Route path='/planets' component={Planets}/>
         <Route exact path='/people' component={People}/>
         <Route  path='/people/:id' component={SinglePerson}/>
         <Route path='/films' component={Films}/>
         <Route exact path='/' component={Home}/>
+
       </Switch>
     </div>
   );
---
+++ src/components/Planets.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Planets extends Component {
+    constructor(){
+        super()
+        
+        this.state = {
+            planets: [],
+        }
+    }
+    componentDidMount(){
+        axios.get('https://swapi.co/api/planets')
+        .then(res => {
+            this.setState({
+                planets: res.data.results
+            })
+        })
+        .catch(err => console.log(err))
+    }
+
+    render(){
+        const { planets } = this.state;
+        return(
+            <>
+                {planets.map(planet => {
+                   return <h2>{planet.name}</h2>
+                })}
+               
+            </>
+        )
+    }
+}
+
+export default Planets;
